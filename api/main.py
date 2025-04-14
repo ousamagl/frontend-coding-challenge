a8405ab361520a4aa12425c2f29a2c111f53113a
oussama galmami <oussama.galmami@gmail.com>
2025-04-14 06:20:13 +0100

    implement basic fast api

---
+++ api/main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, HTTPException, Header, Depends
+from pydantic import BaseModel
+import json
+from typing import List, Optional
+from datetime import datetime
+from jose import JWTError, jwt
+from datetime import timedelta
+import os
+
+app = FastAPI()
+
+SECRET_KEY = "crewmeister"
+
+def load_json(file_name: str):
+    file_path = os.path.join(os.path.dirname(__file__), 'json_files', file_name)
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
+def get_current_user(authorization: str = Header(...)):
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Authorization header is missing")
+    
+    if authorization.startswith("Bearer "):
+        token = authorization[7:].strip()
+    else:
+        raise HTTPException(status_code=401, detail="Invalid authorization code")
+    
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        
+        if decoded_token["exp"] < datetime.utcnow().timestamp():
+            raise HTTPException(status_code=401, detail="Token has expired")
+        
+        return decoded_token["sub"]
+    
+    except JWTError as e:
+        raise HTTPException(status_code=401, detail="Invalid token or token has expired")
+
+
+class Absence(BaseModel):
+    id: int
+    userId: int
+    crewId: int
+    startDate: datetime
+    endDate: datetime
+    admitterId: Optional[int] = None
+    admitterNote: Optional[str] = None
+    createdAt: Optional[datetime] = None
+    confirmedAt: Optional[datetime] = None
+    rejectedAt: Optional[datetime] = None
+    memberNote: Optional[str] = None
+    type: Optional[str] = None
+
+class Member(BaseModel):
+    crewId: int
+    id: int
+    image: str
+    name: str
+    userId: int
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+@app.post("/login")
+async def login(request: LoginRequest):
+    admins = load_json("admin.json")
+    
+    admin = next((admin for admin in admins if admin["username"] == request.username), None)
+    
+    if not admin or admin["password"] != request.password:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+
+    expiration = datetime.utcnow() + timedelta(weeks=1)
+    
+    token = jwt.encode({"sub": request.username, "exp": expiration}, SECRET_KEY, algorithm="HS256")
+    
+    return {"access_token": token}
+
+@app.get("/absences", response_model=List[Absence])
+async def get_absences(skip: int = 0, limit: int = 10, current_user: str = Depends(get_current_user)):
+    absences = load_json("absences.json")
+    return absences[skip: skip + limit]
+
+@app.get("/members", response_model=List[Member])
+async def get_members(current_user: str = Depends(get_current_user)):
+    members = load_json("members.json")
+    return members
